7460ebd010fbe8509e88f4463aa46e7884d482bd
dimonProto <dimonvkedax@gmail.com>
2025-01-30 11:45:20 +0200

    add env and refactor

--- preload.js
+++ preload.js
@@ -1,6 +1,22 @@
+// preload.js
+
+// All the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+      const element = document.getElementById(selector)
+      if (element) element.innerText = text
+    }
+  
+    for (const dependency of ['chrome', 'node', 'electron']) {
+      replaceText(`${dependency}-version`, process.versions[dependency])
+    }
+  })
+
+
 // import { apiKey, playerId } from '/constant.js'
-const apiKey=process.env.API_KEY
-const playerId =process.env.PLAYER_ID
+const apiKey = "f8e59c52-452f-4d11-ae3d-48b71af6a155"
+const playerId = "6036a315-98e4-42cb-9282-2b1fa557048e"
 const playerName = "OpenForce";
 const urlPlayer = `https://open.faceit.com/data/v4/players?nickname=${playerName}`;
 const urlStatsPlayer = `https://open.faceit.com/data/v4/players/${playerId}/stats/cs2`;
@@ -55,7 +71,7 @@ const displayData = async () => {
      <div class="stats-right">
       <div class="stats-elo">
            <p>${data.games.cs2.faceit_elo}</p>
-          <img src="/icons/faceit${data.games.cs2.skill_level}.svg"/>
+          <img src="./icons/faceit${data.games.cs2.skill_level}.svg"/>
       </div>
        <div class="stats-group">
         <div class="stats-group-left">
